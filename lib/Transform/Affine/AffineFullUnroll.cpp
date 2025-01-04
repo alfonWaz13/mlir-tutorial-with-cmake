698ab925a3e4223f302398b1e50522f28fc9830b
alfonWaz13 <alfonwaz13@icloud.com>
2025-01-04 13:16:14 +0100

    feat: add AffineFullUnrollPass runOnOperation implementation

---
+++ lib/Transform/Affine/AffineFullUnroll.cpp
@@ -0,0 +1,22 @@
+#include "AffineFullUnroll.h"
+#include "mlir/Dialect/Affine/IR/AffineOps.h"
+#include "mlir/Dialect/Affine/LoopUtils.h"
+#include "mlir/Pass/Pass.h"
+
+using mlir::affine::AffineForOp;
+using mlir::affine::loopUnrollFull;
+
+namespace mlir {
+namespace tutorial {
+
+void AffineFullUnrollPass::runOnOperation() {
+  getOperation().walk([&](AffineForOp op) {
+    if (failed(loopUnrollFull(op))) {
+      op.emitError("unrolling failed");
+      signalPassFailure();
+    }
+  });
+}
+
+} // namespace tutorial
+} // namespace mlir
